ca0c5f8d091e517d39049c8645d6a6e52ac1b126
Jose Armando Mendez <jmendez@simetricaconsulting.com>
2019-08-19 11:15:29 -0400

    okey

--- AppMVC/Controllers/HomeController.cs
+++ AppMVC/Controllers/HomeController.cs
@@ -1,30 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using AppMVC.Models;
 
 namespace AppMVC.Controllers
 {
+   
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        PruebaDbContext db = new PruebaDbContext();
+
+        public ActionResult Index(Usuarios usu)
         {
-            return View();
+                if (Session["RoleID"] == null)
+                {
+                    return View();
+
+                }
+                return RedirectToAction("Index", "Home");
+
+        }
+        [HttpPost]        
+        public ActionResult login(Usuarios usu)
+        {
+            
+            var creds = db.Usuarios.FirstOrDefault(x => x.UserName == usu.UserName && x.passwd == usu.passwd);
+            if (creds != null)
+            {
+               // FormsAuthentication.SetAuthCookie(creds.UserName, false);
+                var rolid = (from i in db.Usuarios
+                             where usu.UserName == i.UserName && usu.passwd == i.passwd
+                             select i.RoleId).FirstOrDefault();
+                Session["Username"] = usu.UserName;
+                Session["RoleID"] = rolid;
+                Session["LogAlert"] = "Valid";
+                return RedirectToAction("UsuariosIndex", "Usuarios");
+            }
+
+            Session["LogAlert"] = "invalid";            
+            return RedirectToAction("Index","Home");
+            
+        }
+        [Route("Home/logout")]
+        
+        public ActionResult logout()
+        {
+            Session.Abandon();
+            
+            return RedirectToAction("Index");
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+      
     }
 }
